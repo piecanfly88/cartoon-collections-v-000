19014a8733e6f89a274576d77fe7aa1948d618b4
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 08:44:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,10 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf, i| puts "#{i + 1} #{dwarf}"}
 end
-require 'pry'
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|each_call| each_call.capitalize + "!"}
 end
-binding.pry
 
 def long_planeteer_calls# code an argument here
   # Your code here
