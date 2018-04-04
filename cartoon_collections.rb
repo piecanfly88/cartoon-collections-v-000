058eb0510daa3906050174c2010cba0a644b505f
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 08:48:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|each_call| each_call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(assorted_words)
+  assorted_words.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
